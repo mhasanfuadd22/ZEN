31644ecfada9a1a0b44533c00101e343eb2cffaf
mhasanfuadd22 <76463319+mhasanfuadd22@users.noreply.github.com>
2021-01-16 09:11:12 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// untuk menambahkan tinggal copy yang ada terus paste di bawahnya, 
+// ganti kode negara sesuai countrycode.org
+
+$linkOffer = array( 
+    "ALL" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1137140&s3=porn&click_id=ZEN&j1=1&j3=1", // Yang ini jangan dihapus, sebagai default
+    "IN" => "https://www.youtube.com/",
+    "ID" => "https://www.google.com/",
+    "BR" => "https://www.google.com/",
+    "AR" => "https://www.google.com/",
+);
